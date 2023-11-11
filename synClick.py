685334d7019f16f72ab21ef3070cd7136056206f
unknown <fabius8@163.com>
2023-11-11 17:37:56 +0800

    update

--- synClick.py
+++ synClick.py
@@ -34,16 +34,20 @@ class MyListener(mouse.Listener, keyboard.Listener):
     def __init__(self):
         self.keyboard_listener = keyboard.Listener(on_press=self.on_press)
         self.mouse_listener = mouse.Listener(on_click=self.on_click)
+        self.paused = False
 
     def on_click(self, x, y, button, pressed):
         #self.mouse_listener.stop()
+        if self.paused:
+            print("it is paused!")
+            return
         print("on_click", x, y, button, pressed)
         if (x > x_offset) or (y > y_offset):
             print("skip")
             return True
         #time.sleep(0.1)
         if button == mouse.Button.left and not pressed:
-            print("follow click!", x, y)
+            print("follow left click!", x, y)
             # 在所有窗口上点击相同的位置
             for i in range(1, 10):
                 click_x = x + (i % 5) * x_offset
@@ -55,12 +59,20 @@ class MyListener(mouse.Listener, keyboard.Listener):
                 time.sleep(0.1)
                 print(i, "click", click_x, click_y)
             mymouse.position = (x, y)
+            
 
-    def on_press(self, key):
+    def on_press(self, key): # 选择ESC作为暂停、继续键
         if key == keyboard.Key.esc:
-            # 停止监听
-            print("stop")
+            self.paused = not self.paused
+            if self.paused:
+                print("Paused")
+            else:
+                print("Resumed")
+
+        elif key == keyboard.Key.f9:  # 选择F9作为退出键
+            print("Exiting")
             self.mouse_listener.stop()
+            self.keyboard_listener.stop()
             return False
 
     def start(self):
