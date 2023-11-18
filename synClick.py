d4bf6de9eb673aa564589b2c2acc623d90c74f6e
unknown <fabius8@163.com>
2023-11-18 08:43:22 +0800

    update

--- synClick.py
+++ synClick.py
@@ -1,85 +1,142 @@
-import pyautogui
+import win32gui
+import win32con
+import win32api
+import psutil
+import win32process
+import re
 import time
-from pynput import mouse, keyboard
-from pynput.mouse import Button, Controller
 import json
+#import pyautogui
 
 config = json.load(open('config.json'))
-# 获取屏幕大小
 
-screenWidth, screenHeight = pyautogui.size()
-print(screenWidth, screenHeight)
-
-# 4K显示器设置一般放大到150%，因为下面的菜单需要再缩小一部分
-x_change = config["x_change"]
-y_change = config["y_change"]
 k = config["k"]
 
-screenWidth = screenWidth * x_change
-screenHeight = screenHeight * y_change
-print(screenWidth, screenHeight)
+def sort_by_port(cmdline):
+    port_str = next((param for param in cmdline if param.startswith('--remote-debugging-port=')), None)
+    if port_str:
+        port = int(re.search(r'\d+', port_str).group())
+        return port
+    else:
+        return 0 
 
-# 设置Chrome浏览器窗口大小
-chromeWidth = int(screenWidth / 5)
-chromeHeight = int(screenHeight / 2)
+
+def enum_chrome_windows(hwnd, _):
+    classname = win32gui.GetClassName(hwnd)
+    title = win32gui.GetWindowText(hwnd)
+    if classname == 'Chrome_WidgetWin_1' and title:
+        #print(title)
+        chrome_windows.append(hwnd)
 
 
-# 假设每个窗口之间像素的间距, 4K显示器设置一般放大到150%
-x_offset = int(chromeWidth * k)
-y_offset = int(chromeHeight * k)
-print("window size: ", x_offset, y_offset)
-mymouse = Controller()
+# def click(x, y):
+#     print(x, y)
+#     win32api.SetCursorPos((x, y))
+#     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+#     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
 
-class MyListener(mouse.Listener, keyboard.Listener):
-    def __init__(self):
-        self.keyboard_listener = keyboard.Listener(on_press=self.on_press)
-        self.mouse_listener = mouse.Listener(on_click=self.on_click)
-        self.paused = False
+chrome_windows = []
+win32gui.EnumWindows(enum_chrome_windows, None)
 
-    def on_click(self, x, y, button, pressed):
-        #self.mouse_listener.stop()
-        if self.paused:
-            print("it is paused!")
+my_chrome_windows = []
+ports = []
+for hwnd in chrome_windows:
+    print(hwnd)
+    process = psutil.Process(win32process.GetWindowThreadProcessId(hwnd)[1])
+    if any("--remote-debugging-port" in arg for arg in process.cmdline()):
+        print(process.cmdline())
+        port_str = next((param for param in process.cmdline() if param.startswith('--remote-debugging-port=')), None)
+        if port_str:
+            port = int(re.search(r'\d+', port_str).group())
+            ports.append(port)
+            my_chrome_windows.append(hwnd)
+        else:
+            continue
+
+print(ports, my_chrome_windows)
+
+sorted_chrome_windows = [x for _, x in sorted(zip(ports, my_chrome_windows))]
+
+print(sorted_chrome_windows)
+    
+from pynput import mouse, keyboard
+from pynput.keyboard import Key, Controller
+kb = Controller()
+
+for hwnd in sorted_chrome_windows:
+    # 将焦点设置到Chrome窗口
+    print(hwnd)
+    win32gui.SetForegroundWindow(hwnd)
+
+    # 获取窗口的位置和大小
+    rect = win32gui.GetWindowRect(hwnd)
+    x = rect[0]
+    y = rect[1]
+    w = rect[2] - x
+    h = rect[3] - y
+    print(rect)
+    kb.press(Key.alt)
+    kb.release(Key.alt)
+
+    #click(x + w // 2, y + h // 2)
+    #time.sleep(1)
+
+
+# 定义鼠标点击监听函数
+def on_click(x, y, button, pressed):
+    if button == mouse.Button.left and not pressed:  # 监听鼠标左键按下事件
+        print('鼠标左键按下，位置:', x, y)
+        rect = win32gui.GetWindowRect(sorted_chrome_windows[0])
+        rx = rect[0]
+        ry = rect[1]
+        rw = rect[2] - rx
+        rh = rect[3] - ry
+        
+        # 对位置和大小进行150%缩放
+        x_scaled = int(x / k)
+        y_scaled = int(y / k)
+        print(rx, ry, rw, rh, x_scaled, y_scaled)
+        if (x_scaled < rx) or (x_scaled > (rx + rw)) or (y_scaled < ry) or (y_scaled > (ry + rh)):
+            print("not win#1, skip")
             return
-        print("on_click", x, y, button, pressed)
-        if (x > x_offset) or (y > y_offset):
-            print("skip")
-            return True
-        #time.sleep(0.1)
-        if button == mouse.Button.left and not pressed:
-            print("follow left click!", x, y)
-            # 在所有窗口上点击相同的位置
-            for i in range(1, 10):
-                click_x = x + (i % 5) * x_offset
-                click_y = y + (i // 5) * y_offset
-                #print(click_x, click_y)
-                mymouse.position = (click_x, click_y)
-                mymouse.click(Button.left)
-                #mymouse.release(Button.left)
-                time.sleep(0.1)
-                print(i, "click", click_x, click_y)
-            mymouse.position = (x, y)
-            
+        
+        # 相对窗口位置
+        x_related = x_scaled - rx
+        y_related = y_scaled - ry
 
-    def on_press(self, key): # 选择ESC作为暂停、继续键
-        if key == keyboard.Key.esc:
-            self.paused = not self.paused
-            if self.paused:
-                print("Paused")
-            else:
-                print("Resumed")
+        # 在其他9个窗口相对位置执行鼠标按键操作
+        for handle in sorted_chrome_windows:
+            print("handle", handle)
+            if handle == sorted_chrome_windows[0]:
+                continue
 
-        elif key == keyboard.Key.f9:  # 选择F9作为退出键
-            print("Exiting")
-            self.mouse_listener.stop()
-            self.keyboard_listener.stop()
-            return False
+            win32gui.SetForegroundWindow(handle)
+            lParam = win32api.MAKELONG(x_related, y_related)  # 计算鼠标位置参数
+            #win32gui.SendMessage(handle, win32con.WM_ACTIVATE, win32con.WA_ACTIVE, 0)
+            win32api.SendMessage(handle, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, lParam)
+            win32api.SendMessage(handle, win32con.WM_LBUTTONUP, win32con.MK_LBUTTON, lParam)
 
-    def start(self):
-        self.keyboard_listener.start()
-        self.mouse_listener.start()
-        self.keyboard_listener.join()
-        self.mouse_listener.join()
+            #time.sleep(1)
+        
+        #win32api.SetCursorPos((x_related, y_related))
 
-if __name__ == "__main__":
-    MyListener().start()
+# 定义键盘监听函数
+def on_press(key):
+    if key == keyboard.Key.f9:  # 监听F9键按下事件
+        # 退出程序
+        print("exit...")
+        mouse_listener.stop()
+        keyboard_listener.stop()
+        return False
+
+# 创建鼠标监听器
+mouse_listener = mouse.Listener(on_click=on_click)
+mouse_listener.start()
+
+# 创建键盘监听器
+keyboard_listener = keyboard.Listener(on_press=on_press)
+keyboard_listener.start()
+
+# 进入消息循环
+mouse_listener.join()
+keyboard_listener.join()
